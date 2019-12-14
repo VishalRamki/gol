dc7a27eed9931b01241e6cb37b8c1cdb44cc2a5f
Vishal <vishal@local.network>
2019-12-14 15:36:36 -0400

    Public release

---
+++ README.md
@@ -0,0 +1,19 @@
+# Conway's Game Of Life Implementation
+
+This is my implementation of Conway's Game Of Life in Rust + SDL2. It was mostly an excerise in getting SDL2 and rust to play with each other. I'm using this as a jumping off point for a small simulation game I am working on.
+
+I figured people might be interesting in seeing this implmented using rust and Default SDL2.
+
+## Building
+
+1. Install SDL2 Following the instructions at their github page: [https://github.com/Rust-SDL2/rust-sdl2](https://github.com/Rust-SDL2/rust-sdl2)
+2. Clone this repo.
+3. `cargo run` in the repo.
+
+## Release
+
+I included a release on the release page for no real reason. You can download it and try it via a binary if you want.
+
+## License
+
+MIT. I guess.
--- src/main.rs
+++ src/main.rs
@@ -42,23 +42,29 @@ pub fn main() -> Result<(), String> {
     let glider_starting_point_x = 5;
     let glider_starting_point_y = 5;
 
+    // ================================================================
+    // Seed Generation; n = 0;
     grid[glider_starting_point_x + 1][glider_starting_point_y + 2] = 1;
     grid[glider_starting_point_x + 2][glider_starting_point_y + 2] = 1;
     grid[glider_starting_point_x + 3][glider_starting_point_y + 2] = 1;
     grid[glider_starting_point_x + 4][glider_starting_point_y + 2] = 1;
     grid[glider_starting_point_x + 5][glider_starting_point_y + 3] = 1;
     grid[glider_starting_point_x + 4][glider_starting_point_y + 3] = 1;
+    // ================================================================
+
 
     let window_width : i32 = (grid_width as i32 * grid_cell_size as i32) + 1;
     let window_height : i32 = (grid_height as i32 * grid_cell_size as i32) + 1;
 
+    // ===========================================================================
+    // Colour Scheme taken from https://github.com/catsocks/sdl-grid/blob/master/main.c#L27
     let grid_background : Color = Color::RGBA(22, 22, 22, 255); // Barely Black
     let grid_line_colour : Color = Color::RGBA(44, 44, 44, 255); // Dark Gray
     let grid_cursor_ghost_colour : Color = Color::RGBA(44, 44, 44, 255);
     let grid_cursor_colour : Color = Color::RGBA(255, 255, 255, 255);
     // ==========================================
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", window_width as u32, window_height as u32)
+    let window = video_subsystem.window("Simple Naive Implementation of Conway's Game Of Life", window_width as u32, window_height as u32)
         .position_centered()
         .opengl()
         .build()
@@ -83,85 +89,82 @@ pub fn main() -> Result<(), String> {
                 _ => {}
             }
         }
-
-
-                        //===========================
-                    // Perform Game Of Life Simulation/Calculations
-                    
-                    x = 0;
-                    y = 0;
-                    while x < grid_width {
-                        y = 0;
-                        while y < grid_height {
-                            //println!("We Are calculating [{:?},{:?}]", x, y);
-                            // first we calculate the living neighbours of a cell
-                            // using the following
-                            // [-1,-1], [-1, 0], [-1, 1]
-                            // [0, -1], [0, 0],  [0, +1]
-                            // [+1, -1],[+1, 0], [+1, +1]
-                            let mut m_x = 0;
-                            let mut m_y = 0;
-                            let mut n_x = x;
-                            let mut n_y = y;
-                            let c_x = x;
-                            let c_y = y;
-                            let mut living = 0;
-                            for m_x in -1..2 {
-                                for m_y in -1..2 {
-                                    //println!("Looking at location relative [{:?},{:?}]", m_x, m_y);
-                                    
-                                    n_x = match add(n_x, m_x) {
-                                        Some(n) => n,
-                                        None => 0,
-                                    };
-                                    n_y = match add(n_y, m_y) {
-                                        Some(n) => n,
-                                        None => 0,
-                                    };
-                                    //println!("{:?} / {:?}", n_x, n_y);
-                                    if n_x < grid_width && n_y < grid_height {
-                                        if grid[n_x][n_y] == 1 {
-                                            living += 1;
-                                            //println!("Living Entity Found [{:?}, {:?}]", n_x, n_y);
-                                        }
-                                    }
-                                }
-
-                                n_x = c_x;
-                                n_y = c_y;
+        
+        //===========================
+        // Perform Game Of Life Simulation/Calculations
+        
+        x = 0;
+        y = 0;
+        while x < grid_width {
+            y = 0;
+            while y < grid_height {
+                //println!("We Are calculating [{:?},{:?}]", x, y);
+                // first we calculate the living neighbours of a cell
+                // using the following
+                // [-1,-1], [-1, 0], [-1, 1]
+                // [0, -1], [0, 0],  [0, +1]
+                // [+1, -1],[+1, 0], [+1, +1]
+                let mut n_x = x;
+                let mut n_y = y;
+                let c_x = x;
+                let c_y = y;
+                let mut living = 0;
+                for m_x in -1..2 {
+                    for m_y in -1..2 {
+                        //println!("Looking at location relative [{:?},{:?}]", m_x, m_y);
+                        
+                        n_x = match add(n_x, m_x) {
+                            Some(n) => n,
+                            None => 0,
+                        };
+                        n_y = match add(n_y, m_y) {
+                            Some(n) => n,
+                            None => 0,
+                        };
+                        //println!("{:?} / {:?}", n_x, n_y);
+                        if n_x < grid_width && n_y < grid_height {
+                            if grid[n_x][n_y] == 1 {
+                                living += 1;
+                                //println!("Living Entity Found [{:?}, {:?}]", n_x, n_y);
                             }
-                            //println!("Living Cells: {:?}", living);
-                            // Rules taken from https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-                            // cell is alive
-                            if grid[x][y] == 1 {
-                                // Any live cell with two or three live neighbours lives on to the next generation.
-                                if living == 2 || living == 3 {
-                                    grid[x][y] = 1;
-                                }
-                                // Any live cell with more than three live neighbours dies, as if by overpopulation.
-                                else if living > 3 {
-                                    grid[x][y] = 0;
-                                }
-                                // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-                                else {
-                                    grid[x][y] = 0;
-                                }
-                            }
-                            // cell is dead
-                            else if grid[x][y] == 0 {
-                                // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-                                if living == 3 {
-                                    grid[x][y] = 1;
-                                }
-                            }
-
-                            y += 1;
                         }
-                
-                        x += 1;
                     }
-                    
-                    //===========================
+
+                    n_x = c_x;
+                    n_y = c_y;
+                }
+                //println!("Living Cells: {:?}", living);
+                // Rules taken from https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+                // cell is alive
+                if grid[x][y] == 1 {
+                    // Any live cell with two or three live neighbours lives on to the next generation.
+                    if living == 2 || living == 3 {
+                        grid[x][y] = 1;
+                    }
+                    // Any live cell with more than three live neighbours dies, as if by overpopulation.
+                    else if living > 3 {
+                        grid[x][y] = 0;
+                    }
+                    // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                    else {
+                        grid[x][y] = 0;
+                    }
+                }
+                // cell is dead
+                else if grid[x][y] == 0 {
+                    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                    if living == 3 {
+                        grid[x][y] = 1;
+                    }
+                }
+
+                y += 1;
+            }
+    
+            x += 1;
+        }
+        
+        //===========================
         // draw the background
         canvas.set_draw_color(grid_background);
         canvas.clear();
